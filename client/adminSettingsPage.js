f29d9bbe2ebfb969a814b37f6c40c5585fa51a6b
Jin Ding <jinding25@gmail.com>
2015-09-04 09:01:50 -0700

    add save button to adminSettings page

--- client/adminSettingsPage.js
+++ client/adminSettingsPage.js
@@ -39,3 +39,41 @@ Template.templateSettingsBitlyToken.events({
     Session.set("bitlyToken", $('#bitlyToken').val());
   }
 });
+
+Template.settingsInputs.events({
+  'click #buttonSaveSettings': function() {
+      Meteor.call('saveSettings', makeSettingsFromSession(), function (err, res) {
+        if (err) {
+          Session.set('saveError', err.error);
+        } else {
+          console.log('settings saved');
+        }
+      });
+  }
+
+});
+
+setSessionVars = function() {
+	Session.set("orgName", $('#orgName').val());
+	Session.set("akAuth", $('#akAuth').val());
+	Session.set("tagName", $('#tagName').val());
+	Session.set("resourceId", $('#resourceId').val());
+	Session.set("spKey", $('#spKey').val());
+	Session.set("spUrl", $('#spUrl').val());
+	Session.set("bitlyToken", $('#bitlyToken').val());
+};
+
+makeSettingsFromSession = function() {
+  setSessionVars();
+
+  return {
+  	orgName: Session.get('orgName'),
+  	akAuth: Session.get('akAuth'),
+  	tagName: Session.get('tagName'),
+  	resourceId: Session.get('resourceId'),
+  	spKey: Session.get('spKey'),
+  	spUrl: Session.get('spUrl'),
+  	bitlyToken: Session.get('bitlyToken')
+  }
+};
+
