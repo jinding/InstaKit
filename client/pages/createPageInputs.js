20197966fce5fe9a14e5bb03dfc36930cb74a8c2
Jin Ding <jinding25@gmail.com>
2015-09-23 11:04:01 -0700

    change blur to keyup so that Ak snippets insert in right place
    but if typing really really really quickly in the “About text” area,
    the cursor will jump to the bottom of the text.

--- client/pages/createPageInputs.js
+++ client/pages/createPageInputs.js
@@ -20,7 +20,7 @@ Template.templatePageStatement.events({
 });
 
 Template.templatePageAboutText.events({
-  'blur textarea': function() {
+  'keyup textarea': function() {
     Session.set("pageAboutText", $('#pageAboutText').val());
   }
 });
